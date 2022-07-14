0abd7406fe691aa92330273f21a3f5004c4d0e86
Ezequiel Moran <moran.margot@hotmail.com>
2022-07-14 17:22:18 -0300

    api.rest

---
+++ workspace-Curs/api-Rest/apiR.js
@@ -0,0 +1,54 @@
+//Inicializar el proyecto
+//const express = Asignacion de framework
+//require = Importar despendencias
+//('express') = nomnbre de la dependencia
+const express = require('express')
+const mongoose = require('mongoose')
+const user = require('./user.controller')
+const app = express()
+const port = 3000
+
+//Va a tomar todas las peticiones que vengan en formato jSon,
+//la va a trnasformar en un objeto javascript y las va a asignar a la propiedad de body
+app.use(express.json())
+mongoose.connect('mongodb+srv://Ezequiel:magui123@cluster1.djqe0.mongodb.net/miapp?retryWrites=true&w=majority')
+
+
+app.get('/users', user.list) 	
+app.post('/users', user.create)
+app.get('/users/:id', user.get)
+app.put('/users/:id', user.update)
+app.patch('/users/:id', user.update)
+app.delete('/users/:id', user.destroy)
+
+app.use(express.static('app'))
+
+app.get('/', (req, res) => {
+	console.log(__dirname)
+	res.sendFile(`${__dirname}/index.html`)
+})
+
+app.get('*', (req, res) => {
+	res.status(404).send('Esta pagina no existe')
+})
+
+app.listen(port, () => {
+	console.log('Arrancando la aplicacion')
+})
+
+//request: Es donde viene toda las peticiones del cliente
+//response: Sirve para poder enviarle cosas al usuario. EJ: res.send()
+//Endpoint get/list : nos permite que podamos ingresar a esa ruta a travez del explorador
+//GET Recuperar información sobre el recurso API REST
+//status; Nos permite indicarle al cliente(explorador web, o aplicacion movil) si la respuesta tuvo exito y si viene acompañada de un dato 
+//(200) lo vamos a utilizar cuando querramos devolver como resultaodo 'ok', {objet}, [arrays] o  Strings
+//Endpoint post: no se puede acceder poniendo la ruta en la web, se puede utilizar POSTMAN o la terminal
+//Crear un recurso de API REST (Dar de alta)
+//(201) devuelve el valor de 'ok', y 'creado' por ende no es necesario enviarle datos u otras cosas 
+//app.get: creamos este GET para cuando queremos buscar un recurso y no lo queremos listar
+//app.get:params va a devolver un objeto la propiedad de ID
+//app.put: Actualizar (pisar) un recurso de API REST crado con POST
+//app.patch: se utiliza para modificar los valores de las propiedades del recurso
+//app delete: Eliminar un recurso de la API REST o un componente relacionado
+//app.use(express.static): con static le decimos que tiene que ir a buscar en una carpeta 
+//ID primer usuario creado: 62c4b9900aa1ee7b8073d67a
---
+++ workspace-Curs/api-Rest/index.js
@@ -0,0 +1,64 @@
+
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://Ezequiel:magui123@cluster1.djqe0.mongodb.net/miapp?retryWrites=true&w=majority')
+
+//Acciones principales que podemos realizar con los documentos de la libreria de mongoose
+
+const User = mongoose.model('User', {
+	username: String,
+	edad: Number,
+})
+//crear un nuevo usuario
+const crear = async () => {
+	const user = new User({ username: 'MMA', edad: 19})
+	const savedUser = await user.save()
+	console.log(savedUser)
+}
+//crear()
+
+//Buscar todo los usurarios cargados
+const buscarTodo = async () => {
+	const users = await User.find()
+	console.log(users)
+}
+//buscarTodo()
+
+//Buscar un usuario por su nombre 
+const buscar = async () => {//el metodo FIND nos devuelve un arreglo ej: [{ username: 'MMA'}]
+	const user = await User.find({ username: 'MMA'})
+	console.log(user)
+}
+//buscar()
+
+
+//Buscar un usuario devueve un objet 
+const buscarUno = async () => {//el metodo findOne nos devuelve un objet si es que lo encuentra ej: [{ username: 'MMA'}]
+	const user = await User.findOne({ username: 'MMA'})
+	console.log(user)
+}
+//buscarUno()
+
+//Para actualiza lso datos del usuario 
+const actualizar = async () => {
+	const user = await User.findOne({ username: 'MMA'})
+	console.log(user)
+	user.edad = 40// asignamos una nueva edad
+	await user.save()// guardamos el recurso de usuario
+}
+//actualizar()
+
+//Eliminar usuario
+const eliminar = async () => {//el metodo findOne nos devuelve un objet si es que lo encuentra ej: [{ username: 'MMA'}]
+	const user = await User.findOne({ username: 'Ourtown'})
+	console.log(user)
+	if(user)
+	{
+       await user.remove()//solo podemos llamar a remove solo si el usuario existe
+	}
+}
+//eliminar()
+
+//Las funciones asíncronas (async / await) surgen para simplificar el manejo de las promesas. 
+//La palabra async declara una función como asíncrona e indica que una promesa será automáticamente devuelta.
+//Podemos declarar como async funciones con nombre, anónimas o funciones flecha.
